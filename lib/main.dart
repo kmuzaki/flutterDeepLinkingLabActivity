4b8535dbfd8153870de2060375aeda56d48563d7
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-10-08 21:20:46 +0800

    checkpoint4

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class _MyAppState extends State<MyApp> {
   late AppLinks _appLinks;
   StreamSubscription<Uri>? _linkSubscription;
   String _status = 'Waiting for link...';
+  // Keys allow navigation and SnackBar access from anywhere in this State,
+  // including before a BuildContext below MaterialApp is available.
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
   @override
   void initState() {
@@ -28,7 +32,7 @@ class _MyAppState extends State<MyApp> {
     if (initialUri != null) _handleIncomingLink(initialUri);
 
     // 2. Handle links while app is running
-    _linkSubscription = _appLinks.uriLinkStream.listen((Uri uri) {
+    _linkSubscription = _appLinks.uriLinkStream.listen((Uri  uri) {
       _handleIncomingLink(uri);
     }, onError: (err) {
       setState(() => _status = 'Failed to receive link: $err');
@@ -36,15 +40,23 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _handleIncomingLink(Uri uri) {
-    setState(() => _status = 'Received link: $uri');
+    final linkText = 'Received link: ${uri.toString()}';
+    // Debug log for adb logcat
+    print('DeepLink received: ${uri.toString()}');
+    // Update visible status
+    setState(() => _status = linkText);
 
+    // Ensure navigation runs after current frame to avoid navigator/route issues
     if (uri.host == 'details') {
-      // Example link: myapp://details/42
       final id = uri.pathSegments.isNotEmpty ? uri.pathSegments[0] : 'unknown';
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => DetailScreen(id: id)),
-      );
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        // show a SnackBar for immediate feedback using the scaffold key
+        final messengerState = _scaffoldMessengerKey.currentState;
+        messengerState?.showSnackBar(SnackBar(content: Text('Opening details: $id')));
+
+        // navigate using the navigator key
+        _navigatorKey.currentState?.push(MaterialPageRoute(builder: (_) => DetailScreen(id: id)));
+      });
     }
   }
 
@@ -57,6 +69,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigatorKey,
+      scaffoldMessengerKey: _scaffoldMessengerKey,
       title: 'Deep Link Demo',
       home: Scaffold(
         appBar: AppBar(title: Text('Home')),
